3521d0632d46669ea408818f2350127808939450
Florian <florian@nexpose.ch>
2015-08-13 21:38:25 +0200

    fixed some structure bugs

--- pyunicon/util/UCFactory.py
+++ pyunicon/util/UCFactory.py
@@ -3,20 +3,22 @@ from sys import platform as _platform
 __author__ = 'cansik'
 
 
+def get_os_peripherals():
+    if _platform == "linux" or _platform == "linux2":
+        from pyunicon.X11.X11Mouse import X11Mouse
+        from pyunicon.X11.X11Screen import X11Screen
+        return X11Mouse(), None, X11Screen()
+
+    elif _platform == "darwin":
+        print('osx is not support atm!')
+        return None, None, None
+
+    elif _platform == "win32":
+        print('windows is not supported atm!')
+        return None, None, None
+
+
 class UCFactory(object):
-    @staticmethod
-    def get_os_peripherals():
-        if _platform == "linux" or _platform == "linux2":
-            from X11.X11Mouse import X11Mouse
-            from X11.X11Screen import X11Screen
-            return X11Mouse(), None, X11Screen()
-
-        elif _platform == "darwin":
-            print('osx is not support atm!')
-
-        elif _platform == "win32":
-            print('windows is not supported atm!')
-
     @staticmethod
     def get_mouse():
         return UCFactory.__mouse
--- tests/X11MouseTest.py
+++ tests/X11MouseTest.py
@@ -1,2 +1,7 @@
+from pyunicon.UCMouse import UCMouse
+
 __author__ = 'cansik'
 
+mouse = UCMouse()
+print(mouse.get_mouse_position())
+mouse.move_mouse(200, 200)
